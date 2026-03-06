203c529a4b2502fc1ba337104d8fb4a57a5e93ee
Brayan Padilla <padillasilesbrayandavid@gmail.com>
2026-03-05 21:14:06 -0400

    refactor: System model a applications table (key based)

--- app/Models/System.php
+++ app/Models/System.php
@@ -7,9 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class System extends Model
 {
     protected $connection = 'core';
-    protected $table = 'systems';
+    protected $table = 'applications';
 
-    protected $fillable = ['name', 'display_name', 'description', 'url', 'icon', 'active'];
+    protected $fillable = ['key', 'nombre', 'descripcion', 'url', 'icono', 'color', 'activo'];
+
+    public function getNameAttribute()
+    {
+        return $this->key;
+    }
+
+    public function getDisplayNameAttribute()
+    {
+        return $this->nombre;
+    }
 
     protected $casts = [
         'active' => 'boolean',
